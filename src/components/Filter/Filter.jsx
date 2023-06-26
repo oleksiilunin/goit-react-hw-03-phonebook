3e9fc328465b93300a18cc246070265dab634550
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-06-26 20:47:40 +0300

    fix

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -19,7 +19,6 @@ const Filter = ({ filterValue, onChangeInput }) => {
         value={filterValue}
         pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        autoFocus
         required
         onChange={onChangeInput}
       />
